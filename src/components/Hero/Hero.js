a3ef48e2f7b3b4cfd645414acf13be14bd18c82a
josedebuck <debuckjose@gmail.com>
2023-09-21 16:32:56 -0300

    upload

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import Dropdown from "../Dropdown/Dropdown";
+import Header from "../Header/Header";
+import {
+  HeroContainer,
+  HeroWrapper,
+  HeroLeft,
+  HeroRight,
+  Image,
+  ScrollDown,
+  ScrollLink,
+} from "./HeroElements.js";
+function Hero() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+  return (
+    <main>
+      <Dropdown isOpen={isOpen} toggle={toggle} />
+      <Header toggle={toggle} />
+      <HeroContainer>
+        <HeroWrapper>
+          <HeroLeft>
+            <h1>Hi, I'm José Debuck</h1>
+            <h5>Fullstack Developer</h5>
+            <p>
+              I design and code beautifully simple things, and I love what I do.
+            </p>
+          </HeroLeft>
+          <HeroRight>
+          <Image src="/portfolio.jpg" alt="Imagen de mi" />
+          </HeroRight>
+        </HeroWrapper>
+        <ScrollDown to="projects">
+          <ScrollLink>
+            Scroll down
+            <img
+              src="https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/scroll-down.svg"
+              alt="scroll-down"
+            />
+          </ScrollLink>
+        </ScrollDown>
+      </HeroContainer>
+    </main>
+  );
+}
+
+export default Hero;
